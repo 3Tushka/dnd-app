2b0af42d16c57111dec6fc17c9cf5730c3e2dedd
Andriy <andriypanasyk02@gmail.com>
2024-07-29 15:48:34 +0300

    feat: classes changes

--- src/app/classes/classes.component.ts
+++ src/app/classes/classes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ClassesService } from "./classes.service";
 import { BehaviorSubject, switchMap } from "rxjs";
 import { ClassInterface } from "./classes.interface";
+import { classes } from "./classes.filler.data";
 
 @Component({
   selector: "app-classes",
@@ -9,11 +10,16 @@ import { ClassInterface } from "./classes.interface";
   styleUrls: ["./classes.component.scss"],
 })
 export class ClassesComponent implements OnInit {
-  currentClass = new BehaviorSubject<string>("");
+  classes: { name: string; backgroundImageUrl: string; text: string }[] = [];
 
-  ngOnInit() {}
+  classNames = classes;
 
-  selectClass(className: string) {
-    this.currentClass.next(className);
+  ngOnInit() {
+    this.classNames.sort();
+    this.classes = this.classNames.map((className) => ({
+      name: className.name,
+      text: className.text,
+      backgroundImageUrl: `url("../../assets/images/class/${className.name}.png")`,
+    }));
   }
 }
---
+++ src/app/classes/classes.filler.data.ts
@@ -0,0 +1,61 @@
+export const classes = [
+  {
+    name: "bard",
+    text: "An inspiring magician whose power echoes the music of creation",
+  },
+
+  {
+    name: "warlock",
+    text: "A wielder of magic that is derived from a bargain with an extraplanar entity",
+  },
+
+  {
+    name: "wizard",
+    text: "A scholarly magic-user capable of manipulating the structures of reality",
+  },
+
+  {
+    name: "barbarian",
+    text: "A fierce warrior who can enter a battle rage",
+  },
+
+  {
+    name: "rogue",
+    text: "A scoundrel who uses stealth and trickery to overcome obstacles and enemies",
+  },
+
+  {
+    name: "paladin",
+    text: "A holy warrior bound to a sacred oath",
+  },
+
+  {
+    name: "druid",
+    text: "A priest of the Old Faith, wielding the powers of nature and adopting animal forms",
+  },
+
+  {
+    name: "fighter",
+    text: "A master of martial combat, skilled with a variety of weapons and armor",
+  },
+
+  {
+    name: "ranger",
+    text: "A warrior who combats threats on the edges of civilization",
+  },
+
+  {
+    name: "sorcerer",
+    text: "A spellcaster who draws on inherent magic from a gift or bloodline",
+  },
+
+  {
+    name: "monk",
+    text: "A master of martial arts, harnessing the power of the body in pursuit of physical and spiritual perfection",
+  },
+
+  {
+    name: "cleric",
+    text: "A priestly champion who wields divine magic in service of a higher power",
+  },
+];
